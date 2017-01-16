981aa1e872fe810a2a412840cfb0684ce9de658e
root <root@debian>
2017-01-16 17:48:06 +0800

    更新kube项目名称

--- update_Dockfile.sh
+++ update_Dockfile.sh
@@ -2,10 +2,11 @@ while read line; do
 	#echo $line
 	project_name=`echo $line |awk '{split($0,a,":");print a[1]}'`
 	project_version=`echo $line |awk '{split($0,a,":");print a[2]}'`
-	if [ ! -d "$project_name" ]
+	if [ -d "$project_name" ]
 	then
-		mkdir $project_name 
+		echo "FROM gcr.io/google_containers/$project_name:$project_version
+MAINTAINER cmm cmm@mmcloud.io"> "$project_name/Dockerfile"
+	else
+		echo "$protject_name not found"
 	fi
-	echo "FROM gcr.io/google_containers/kube-apiserver-amd64:$project_version
-MAINTAINER mingmingcong mingming@dong-design.com"> "$project_name/Dockerfile"
 done < README 
