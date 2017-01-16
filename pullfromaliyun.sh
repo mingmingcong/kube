01ad51ed08b71642c1fe8b6c970da5be2c3851fd
cmm <cmm@mmcloud.io>
2017-01-16 19:14:23 +0800

    update pull

---
+++ pullfromaliyun.sh
@@ -0,0 +1,8 @@
+domain="registry.cn-beijing.aliyuncs.com/mmcloud"
+while read line; do
+	project_name=`echo $line |awk '{split($0,a,":");print a[1]}'`
+	project_version=`echo $line |awk '{split($0,a,":");print a[2]}'`
+	docker pull $domain/$project_name:$project_version
+	docker tag $domain/$project_name:$project_version gcr.io/google_containers/$project_name:$project_version
+	docker rmi $domain/$project_name:$project_version
+done < README
